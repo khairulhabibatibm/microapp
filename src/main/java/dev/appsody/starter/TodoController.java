d4b3d9b19effce386707773926d30bb74c0fa6f5
Khairul Habib <khairul.habib@ibm.com>
2020-03-23 15:19:07 +0700

    add circuit breaker controller

--- src/main/java/dev/appsody/starter/TodoController.java
+++ src/main/java/dev/appsody/starter/TodoController.java
@@ -1,15 +1,27 @@
 package dev.appsody.starter;
 
-import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Timeout;
+
 @Path("/todo")
-@Singleton
 public class TodoController{
 
     @GET
+    @Timeout(200L)
+    @Fallback(fallbackMethod = "listLocal")
     public String list(){
+        try{
+            Thread.sleep(700L);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
         return "This is todo list";
+    }
+
+    public String listLocal(){
+        return "This is list from local";
     }
 }
